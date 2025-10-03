ffe23f759da215d788879085c88037712bdfe5e3
samtomalau8-hash <samtomalau8@gmail.com>
2025-10-03 14:44:12 +0700

    Pair 4

---
+++ T00.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.lang.Math;
+
+public class T00 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a, b, c, d;
+
+        System.out.println("Masukkan bilangan pertama");
+        a = Integer.parseInt(input.nextLine());
+        System.out.println("Masukkan bilangan kedua");
+        b = Integer.parseInt(input.nextLine());
+        if (a % 2 == 0) {
+            System.out.println("Bilangan pertama: Genap");
+        } else {
+            System.out.println("Bilangan pertama: Ganjil");
+        }
+        if (b % 2 == 0) {
+            System.out.println("Bilangan kedua: Genap");
+        } else {
+            System.out.println("Bilangan kedua: Ganjil");
+        }
+        if (a == b) {
+            System.out.println("Kedua bilangan sama besar");
+        } else {
+            if (a < b) {
+                System.out.println("Bilangan kedua lebih besar");
+            } else {
+                System.out.println("Bilangan pertama lebih besar");
+            }
+        }
+        if (a % 2 == 0 && b % 2 == 0) {
+            c = a + b;
+            System.out.println("Hasil penjumlahan:" + " " + c);
+        } else {
+            if (b + 1 % 2 == 0 && a + 1 % 2 == 0) {
+                d = a * b;
+                System.out.println("Hasil perkalian:" + " " + d);
+            } else {
+                System.out.println("berbeda jenis");
+            }
+        }
+    }
+}
